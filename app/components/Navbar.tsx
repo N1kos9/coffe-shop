c0b7c327f0d3388687b1b53d9cc6481c71e1a26c
N1kos9 <onkl3p@gmail.com>
2024-03-01 21:49:27 +0200

    theme provider added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,47 +4,33 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-
+import { ModeToggle } from "@/components/Toggle";
 export default function Navbar() {
-  const [theme, setTheme] = useState("dark"); // Default theme is set to 'dark'
-
-  const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === "dark" ? "light" : "dark"));
-    document.documentElement.setAttribute("data-theme", theme); // Toggle theme attribute in HTML
-  };
-
   // Additional styles for light theme
-  const navbarStyles =
-    theme === "dark" ? "bg-black text-white" : "bg-white text-black";
-  const linkHoverStyle =
-    theme === "dark" ? "hover:text-gray-300" : "hover:text-gray-700";
-
   return (
-    <nav className={`flex justify-between items-center p-4 ${navbarStyles}`}>
+    <nav className={`flex justify-between items-center p-4 `}>
       <div className="text-lg font-bold">Coffee Shop</div>
       <div>
-        <Link href="/" className={`px-4 ${linkHoverStyle}`}>
+        <Link href="/" className={`px-4 `}>
           Home
         </Link>
-        <Link href="/menu" className={`px-4 ${linkHoverStyle}`}>
+        <Link href="/menu" className={`px-4 `}>
           <Button>Menu</Button>
         </Link>
-        <Link href="/about" className={`px-4 ${linkHoverStyle}`}>
+        <Link href="/about" className={`px-4 `}>
           About Us
         </Link>
-        <Link href="/blog" className={`px-4 ${linkHoverStyle}`}>
+        <Link href="/blog" className={`px-4 `}>
           Blog
         </Link>
-        <Link href="/contact" className={`px-4 ${linkHoverStyle}`}>
+        <Link href="/contact" className={`px-4 `}>
           Contact
         </Link>
-        <Link href="/faq" className={`px-4 ${linkHoverStyle}`}>
+        <Link href="/faq" className={`px-4 `}>
           FAQ
         </Link>
         {/* Theme Toggle Button */}
-        <button onClick={toggleTheme} className="ml-4 px-4 py-2 border rounded">
-          {theme === "dark" ? "Light Mode" : "Dark Mode"}
-        </button>
+        <ModeToggle />
       </div>
     </nav>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { ThemeProvider } from "./components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,9 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
