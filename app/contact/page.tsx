f5080b09c161d4ee6245c11d74b63e8ada475a5a
N1kos9 <onkl3p@gmail.com>
2024-03-05 23:58:31 +0200

    better contact us page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,82 +1,92 @@
+"use client";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Implementation for submitting the form data
+    alert("Thank you for your message. We will get back to you shortly!");
+    // Reset the form fields after submission
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <Layout>
-      <div className="px-5 py-10 md:p-10">
-        <h2 className="text-3xl font-bold text-center">Contact Us</h2>
-        <p className="text-center mt-4 mb-8">
-          Have questions or feedback? We'd love to hear from you. Fill out the
-          form below or reach out via email or phone.
-        </p>
-
-        <div className="max-w-lg mx-auto bg-white shadow-lg rounded-lg overflow-hidden p-5">
-          <div className="flex flex-col items-center">
-            <div className="text-center">
-              <p className="font-semibold">Email: contact@coffeeshop.com</p>
-              <p className="font-semibold mb-4">Phone: (123) 456-7890</p>
+      <div className="flex flex-col items-center justify-center min-h-screen p-4">
+        <div className="w-full max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-8">
+          <h2 className="text-4xl font-bold text-center mb-8">Contact Us</h2>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                required
+                className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-black focus:border-black"
+                placeholder="Your Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
-            <form className="w-full max-w-lg">
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3 mb-6 md:mb-0">
-                  <label
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    htmlFor="grid-first-name"
-                  >
-                    Name
-                  </label>
-                  <input
-                    className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                    id="grid-first-name"
-                    type="text"
-                    placeholder="Jane Doe"
-                  />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                  <label
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    htmlFor="grid-email"
-                  >
-                    Email
-                  </label>
-                  <input
-                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="grid-email"
-                    type="email"
-                    placeholder="email@coffeeshop.com"
-                  />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                  <label
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    htmlFor="grid-message"
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-                    id="grid-message"
-                    placeholder="Your message..."
-                  ></textarea>
-                </div>
-              </div>
-              <div className="md:flex md:items-center">
-                <div className="md:w-1/3">
-                  <button
-                    className="shadow bg-black hover:bg-gray-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                    type="button"
-                  >
-                    Send
-                  </button>
-                </div>
-                <div className="md:w-2/3"></div>
-              </div>
-            </form>
-          </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email Address
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-black focus:border-black"
+                placeholder="you@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows="4"
+                required
+                className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-black focus:border-black"
+                placeholder="Write your message here..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+              >
+                Send Message
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </Layout>
