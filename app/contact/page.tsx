c51a1ef223361e03f267137ae3612eae0829ed22
N1kos9 <onkl3p@gmail.com>
2024-03-05 23:51:06 +0200

    contact us page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,14 +3,80 @@ import Layout from "../components/Layout";
 export default function Contact() {
   return (
     <Layout>
-      <div className="p-10">
+      <div className="px-5 py-10 md:p-10">
         <h2 className="text-3xl font-bold text-center">Contact Us</h2>
-        <p className="text-center mt-4">
-          Have questions or feedback? We'd love to hear from you.
+        <p className="text-center mt-4 mb-8">
+          Have questions or feedback? We'd love to hear from you. Fill out the
+          form below or reach out via email or phone.
         </p>
-        <div className="flex flex-col items-center mt-6">
-          <p>Email: contact@coffeeshop.com</p>
-          <p>Phone: (123) 456-7890</p>
+
+        <div className="max-w-lg mx-auto bg-white shadow-lg rounded-lg overflow-hidden p-5">
+          <div className="flex flex-col items-center">
+            <div className="text-center">
+              <p className="font-semibold">Email: contact@coffeeshop.com</p>
+              <p className="font-semibold mb-4">Phone: (123) 456-7890</p>
+            </div>
+            <form className="w-full max-w-lg">
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3 mb-6 md:mb-0">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-first-name"
+                  >
+                    Name
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                    id="grid-first-name"
+                    type="text"
+                    placeholder="Jane Doe"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-email"
+                    type="email"
+                    placeholder="email@coffeeshop.com"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="grid-message"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                    id="grid-message"
+                    placeholder="Your message..."
+                  ></textarea>
+                </div>
+              </div>
+              <div className="md:flex md:items-center">
+                <div className="md:w-1/3">
+                  <button
+                    className="shadow bg-black hover:bg-gray-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                    type="button"
+                  >
+                    Send
+                  </button>
+                </div>
+                <div className="md:w-2/3"></div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </Layout>
